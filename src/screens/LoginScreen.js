f41cbb7c78e32299336bde5fc8a6f1c675b76859
Shabnam <143620346+shabnam-tech@users.noreply.github.com>
2024-11-09 00:03:25 +0530

    icon added

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 
 const LoginScreen = ({ navigation }) => {
   const handleRoleSelection = (role) => {
-    if (role === 'TCEian') {
+    if (role === 'Student/Faculty') {
       navigation.navigate('StudentOrFacultyScreen', { role });
     } else {
       navigation.navigate('ParentOrVisitorScreen', { role });
@@ -12,9 +12,18 @@ const LoginScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
+      {/* TCE Icon */}
+      <Image
+        source={require('../../assets/tce.png')} 
+        style={styles.icon}
+      />
+
+      {/* Header */}
       <Text style={styles.header}>Who are you?</Text>
-      <TouchableOpacity style={styles.button} onPress={() => handleRoleSelection('TCEian')}>
-        <Text style={styles.buttonText}>TCEian</Text>
+
+      {/* Role selection buttons */}
+      <TouchableOpacity style={styles.button} onPress={() => handleRoleSelection('Student/Faculty')}>
+        <Text style={styles.buttonText}>TCE Student / Faculty</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.button} onPress={() => handleRoleSelection('parent')}>
         <Text style={styles.buttonText}>Parent</Text>
@@ -28,6 +37,7 @@ const LoginScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  icon: { width: 100, height: 100, marginBottom: 20 }, // Adjust the size as per your preference
   header: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
   button: { backgroundColor: '#4CAF50', padding: 15, marginVertical: 10, width: '80%', alignItems: 'center', borderRadius: 8 },
   buttonText: { color: '#fff', fontSize: 18 },
